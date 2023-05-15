02ce9c82079d774eed4d28dbd2f54703d7f29f9a
Mattias <mattiaswelamsson@gmail.com>
2023-05-16 05:23:00 +0900

    converting logic works

--- currency_converter.rb
+++ currency_converter.rb
@@ -4,25 +4,38 @@ require 'glimmer-dsl-libui'
 include Glimmer
 
 data = [
-  ['USD', 10, 0],
-  ['SEK', 1, 0],
-  ['JPY', 0.1, 0],
+  ['USD', 1, 0],
+  ['EUR', 0.923466, 0],
+  ['SEK', 10.45, 0],
+  ['JPY', 137.271, 0],
+  ['WON', 1336.50, 0],
 ]
 
 window('Currency Converter', 600, 500) {
   vertical_box {
     table {
       text_column('Currency')
-      text_column('Current Value')
+      text_column('Market Value')
       text_column('Converted Value')
 
        cell_rows data
     }
 
-    button('Calculate') {
+    search_entry { |value|
       stretchy false
-      on_clicked do
-        puts "Click click"
+
+      on_changed do
+        new_value = value.text
+        new_data ||= data.dup
+
+        # Loopa över alla valutor
+        # Byta ut värdet på valuta[2] till ett nytt värde
+        # Uppdatera värdena i orginal-data
+        new_data.each do |currency|
+          currency[2] = 0
+          new_total = (currency[2]) + new_value.to_i * currency[1]
+          currency[2] = new_total.round(2)
+        end
       end
     }
 
