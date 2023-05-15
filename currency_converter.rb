ea835d42e9fbfab92057ad492bdca0ac98af9d1e
Mattias <mattiaswelamsson@gmail.com>
2023-05-16 01:57:31 +0900

    add button

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,20 +1,30 @@
+# docs: https://github.com/AndyObtiva/glimmer-dsl-libui#area-text
 require 'glimmer-dsl-libui'
 
 include Glimmer
 
 data = [
   ['USD', 10, 0],
-  ['SEK', 1, 0]
+  ['SEK', 1, 0],
+  ['JPY', 0.1, 0],
 ]
 
-window('Currency Converter', 300, 200) {
+window('Currency Converter', 600, 500) {
   vertical_box {
     table {
       text_column('Currency')
       text_column('Current Value')
       text_column('Converted Value')
+
+       cell_rows data
     }
 
-    cell_rows data
+    button('Calculate') {
+      stretchy false
+      on_clicked do
+        puts "Click click"
+      end
+    }
+
   }
 }.show
